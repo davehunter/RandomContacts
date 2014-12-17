c6bd536f60f705c59fc52cf24a93cddc33130632
Dave Hunter <dave@bendyware.com>
2014-12-17 11:41:27 -0500

    Initial basic iPhone project

---
+++ RandomContacts/RandomContacts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RandomContacts
+//
+//  Created by Dave Hunter on 2014-12-17.
+//  Copyright (c) 2014 Dave Hunter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RandomContacts/RandomContacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RandomContacts
+//
+//  Created by Dave Hunter on 2014-12-17.
+//  Copyright (c) 2014 Dave Hunter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
